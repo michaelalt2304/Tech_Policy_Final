73c5c91de36237aa762cb07eb1065e74fa0e442c
Michael Straus <mjstraus2304@gmail.com>
2025-04-08 15:27:10 -0400

    Start with check boxes

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("Final Project for Policy in Privacy Technologies")
+stuff = st.text_input("What are you looking for in a privacy tool?")
+st.write(stuff)
+st.checkbox("Public?")
+st.selectbox("Choices for Privacy", ["A", "B", "C"])
+print(stuff)
